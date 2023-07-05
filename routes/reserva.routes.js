8e6c7f775133ed5abab4c359b131d51cd6604244
yulimereles <yuuumereles@gmail.com>
2023-07-05 14:55:23 -0300

    agregue cambios en app.js, la importacion de las diferentes depedencias, la importacion de la bases de  datos y conexion a bases de datos, por otro lado los middelewares, routes, etc

--- routes/reserva.routes.js
+++ routes/reserva.routes.js
@@ -1,33 +1,48 @@
-// TODO: Importar el modelo y controladores de reservas, luego vincular rutas con controladores
+// TODO: Importar controladores de reservas, luego vincular rutas con controladores
 
-const router = require('express').Router();
+const { Router } = require("express");
+const router = Router();
 
+const {
+  renderListaReservas,
+  renderFormNuevaReserva,
+  renderFormEditarReserva,
+  obtenerReservas, // Obtener todas
+  obtenerReserva, // Obtener un única reserva
+  crearReserva,
+  actualizarReserva,
+  eliminarReserva,
+} = require("../controllers/reserva.controllers");
 
 // ==========================================
 //         Rutas para renderizar vistas
 // ==========================================
 
-// Obtener todas las reservas
+// Obtener todas las reservas localhost:4000/
+router.get("/", renderListaReservas);
 
 // Formulario para crear una reserva
-
-// Formulario para actualizar una reserva
+router.get("/crear-reserva", renderFormNuevaReserva);
 
 // ==========================================
 //         Rutas para CRUD de reservas
 // ==========================================
 
 // Obtener todas las reservas
-router.get('/api/',);
- 
-// Crear una reserva
-router.post('/api/',);
- 
-// Actualizar una reserva
-router.put('/api/:id',);
- 
-// Eliminar una reserva de forma lógica
-router.delete('/api/:id',);
+router.get("/api", obtenerReservas);
 
- 
- module.exports = router;
+// Crear una reserva
+router.post("/api", crearReserva);
+
+router.get("/api/:id", obtenerReserva);
+
+// Actualizar una reserva
+router.put("/api/:id", actualizarReserva);
+
+// Eliminar una reserva de forma lógica
+router.delete("/api/:id", eliminarReserva);
+
+// Formulario para actualizar una reserva
+router.get("/actualizar-reserva/:id", renderFormEditarReserva); // para capturar :id -> req.params.id
+
+module.exports = router;
