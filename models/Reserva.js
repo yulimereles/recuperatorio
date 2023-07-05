0edd4f0d63b8b825a96e6c301181e24b8e7148f0
yulimereles <yuuumereles@gmail.com>
2023-07-05 15:02:59 -0300

    agregue el modelo de datos para la reserva de vuelo

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,84 @@
 // TODO: Crear modelo de datos de Reserva
+
+const { DataTypes, sequelize } = require("../database");
+
+const Reserva = sequelize.define(
+  "reserva",
+  {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    codigo: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      defaultValue: new Date().getTime(),
+    },
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    fecha_ingreso: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    fecha_salida: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    habitacion: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    cantidad_personas: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    telefono: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    estado: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: "reservas",
+  }
+);
+
+// Crear tabla si no existe ({force: true} borra y crea la tabla)
+Reserva.sync({ force: false }).then(() => {
+  console.log("Tabla de Reservas creada");
+});
+
+module.exports = Reserva;
